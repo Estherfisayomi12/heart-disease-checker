16d4a71cdf9e57f27dea4e3cf82439cb1a3a8568
Estherfisayomi12 <fisayomi2602@gmail.com>
2022-07-06 16:12:44 +0100

    Made changes to CSS

---
+++ app.py
@@ -0,0 +1,26 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+# create flask app
+app = Flask(__name__)
+
+# load the model
+model = pickle.load(open('logistics_regression_model.pkl', "rb"))
+
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+
+@app.route('/predict', methods=["POST"])
+def predict():
+    float_features = [float(x) for x in request.form.values()]
+    features = [np.array(float_features)]
+    prediction = model.predict(features)
+    return render_template('index.html', Result='Heart disease present'if prediction[0] == 0 else 'Heart Disease free')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
